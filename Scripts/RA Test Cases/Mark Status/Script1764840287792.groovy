fa7088af47d078ad6383b8d1c659e0f28159f993
manhuakatalon <man.hua@katalon.com>
2025-12-04 16:44:33 +0700

    update

--- Scripts/RA Test Cases/Mark Status/Script1764840287792.groovy
+++ Scripts/RA Test Cases/Mark Status/Script1764840287792.groovy
@@ -1,3 +1,6 @@
 import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.delay(300)
 
 KeywordUtil.markFailedAndStop("Failed")
--- Test Suites/RA/Mark Status.ts
+++ Test Suites/RA/Mark Status.ts
@@ -5,47 +5,12 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>1</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>d9719d8a-83b0-4f2f-847d-cac8a29a0ea1</testSuiteGuid>
-   <testCaseLink>
-      <guid>ecb8f5b1-44f9-4926-bcca-82579ff75736</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/RA Test Cases/Mark Status</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b0eeb539-899d-42d4-b690-47fb65cedbf5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/RA Test Cases/Mark Status</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>2830f506-494d-4236-8192-706bbfef0a8d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/RA Test Cases/Mark Status</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0392e9ab-cf8c-4966-ad81-70b9d808c27b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/RA Test Cases/Mark Status</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>87902913-2213-4b57-93ed-c94342cc508e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/RA Test Cases/Mark Status</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
    <testCaseLink>
       <guid>4a3b9c55-30f7-439b-ade7-69979d19205d</guid>
       <isReuseDriver>false</isReuseDriver>
