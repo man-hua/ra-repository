54c3cbc699a877d917d6b863507bf9567026bae4
manhuakatalon <man.hua@katalon.com>
2025-12-11 13:22:08 +0700

    test

--- Scripts/RA Test Cases/Multiple Status/Test Case - Pass/Script1765182478876.groovy
+++ Scripts/RA Test Cases/Multiple Status/Test Case - Pass/Script1765182478876.groovy
@@ -1,5 +1,5 @@
 import com.kms.katalon.core.util.KeywordUtil
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-WebUI.delay(30)
+WebUI.delay(60)
 KeywordUtil.markFailedAndStop("New Failure for Pass test case")
 KeywordUtil.markPassed("Passed")
