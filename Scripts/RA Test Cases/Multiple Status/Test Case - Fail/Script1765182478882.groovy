4046977bd057fbe6cf2df514bacd278d0c269b00
manhuakatalon <man.hua@katalon.com>
2026-01-19 13:09:58 +0700

    add

--- Scripts/RA Test Cases/Multiple Status/Test Case - Fail/Script1765182478882.groovy
+++ Scripts/RA Test Cases/Multiple Status/Test Case - Fail/Script1765182478882.groovy
@@ -1,8 +1,8 @@
 import com.kms.katalon.core.util.KeywordUtil
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-//KeywordUtil.markFailed("Failed")
-WebUI.delay(10)
+KeywordUtil.markFailed("Failed")
+
 KeywordUtil.logInfo("Log info 1")
 
 KeywordUtil.logInfo("Log info 2")
